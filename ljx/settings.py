1026c21983add39216896cb3c342015791714aee
jeeyshe <jeeyshe@gmail.com>
2019-05-07 21:28:41 +0800

    优化已登录用户体验

--- ljx/settings.py
+++ ljx/settings.py
@@ -92,7 +92,6 @@ MIDDLEWARE = [
     # 以下是自定义中间件
     'db.my_middlewares.AllMethodSupportMiddleware',
     'db.my_middlewares.VisitCountMiddleware',
-    # 'db.my_middlewares.LinkClickMiddleware',
 ]
 
 ROOT_URLCONF = 'ljx.urls'

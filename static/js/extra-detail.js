4fe4cf84b09bdb642746dee36ddeaa109da322fe
jeeyshe <jeeyshe@gmail.com>
2019-05-18 23:00:35 +0800

    修复代码块宽度超出bug

--- static/js/extra-detail.js
+++ static/js/extra-detail.js
@@ -1,8 +1,11 @@
-//解决文章详情页ueditor代码块宽度超出问题
-$('.syntaxhighlighter').each(function (index, item) {
-    var s = $('<div style="overflow-x: auto" class="codeBox"></div>');
-    var html = $(item).clone()[0].outerHTML;
-    $(item).before(s);
-    $(item).remove();
-    $('.codeBox').eq(index).html(html);
+$(document).ready(function () {
+    //解决文章详情页ueditor代码块宽度超出问题
+    $('.syntaxhighlighter').each(function (index, item) {
+        var s = $('<div style="overflow-x: auto" class="codeBox"></div>');
+        var html = $(item).clone()[0].outerHTML;
+        $(item).before(s);
+        $(item).remove();
+        $('.codeBox').eq(index).html(html);
+    });
+
 });

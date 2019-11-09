88df7e4fd0dc64c1796d100cc882c80d40c91f3e
jeeyshe <jianxin.lu@woqutech.com>
2019-11-09 14:43:18 +0800

    异步任务暂存

--- tasks/mail.py
+++ tasks/mail.py
@@ -4,12 +4,13 @@
 """
  邮件异步任务
 """
+from smtplib import SMTP_SSL
 
-import time
 import celery
+from ljx.settings import (EMAIL_HOST, EMAIL_HOST_PASSWORD, EMAIL_HOST_USER, EMAIL_PORT, EMAIL_USE_SSL, EMAIL_USE_TLS,
+                          EMAIL_SUBJECT_PREFIX)
 
 
 @celery.task(name="tasks.mail.send_message")
 def send_message():
-    time.sleep(7)
     print("=====>send___777")

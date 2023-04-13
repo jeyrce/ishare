38794c2394fef57b4ec6d53f8d86aef2e011d31f
jeyrce <jeyrce@gmail.com>
2023-04-13 14:47:31 +0800

    docs: 提示信息更换
    - 1、微信公众号图片替换
    - 2、提示信息变更

--- static/js/egg.js
+++ static/js/egg.js
@@ -48,7 +48,7 @@ function doing() {
 }
 
 $(document).ready(function () {
-    layer.msg('<h1>网站已归档不再更新内容,欢迎关注新站: </h1><h1>imseek.cn</h1>', {
+    layer.msg('<h1>网站已归档不再更新内容,欢迎关注微信公众号: </h1><h1>【不止平凡】</h1>', {
         time: 3000, //3s后自动关闭
     });
     console.clear();
@@ -60,6 +60,8 @@ $(document).ready(function () {
     console.log('\r\n');
     console.log("%c2019-04-09 00:40 http://www.lujianxin.com", "color: green;");
     console.log("%c~既然都发现这个彩蛋了，收藏一下本站呗^o^", "color: red;");
-    console.log("%c> 本站从2022-01-01日起不再维护和更新内容，欢迎前往新站: https://imseek.cn", "color: green;");
+    console.log("%c> 本站从2022-01-01日起不再维护和更新内容，欢迎微信关注公众号: 【不止平凡】", "color: green;");
+    console.log("%c> 本站从2022-01-01日起不再维护和更新内容，欢迎微信关注公众号: 【不止平凡】", "color: green;");
+    console.log("%c> 本站从2022-01-01日起不再维护和更新内容，欢迎微信关注公众号: 【不止平凡】", "color: green;");
 });
 
